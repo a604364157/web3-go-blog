16048d69d2efa94250e4d6d07fed42a7b84752f6
jiangjx <eddie.jiang@ly.com>
2025-09-29 17:34:51 +0800

    init

--- tests/blog_test.go
+++ tests/blog_test.go
@@ -43,6 +43,7 @@ func register(name string) {
 	fmt.Println(w)
 }
 
+// TestLogin 登录测试
 func TestLogin(t *testing.T) {
 	fmt.Println(login("zhangsan", "123456"))
 }
@@ -58,6 +59,7 @@ func login(username, password string) string {
 	return res["data"].(string)
 }
 
+// TestCreatePost 创建文章测试
 func TestCreatePost(t *testing.T) {
 	token := login("zhangsan", "123456")
 	body := bytes.NewBuffer([]byte(`{"title":"这是一篇测试文章","content":"这是一篇测试文章的内容"}`))
@@ -69,6 +71,7 @@ func TestCreatePost(t *testing.T) {
 	fmt.Println(w.Body.String())
 }
 
+// TestListPost 列出文章测试
 func TestListPost(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/api/posts", nil)
 	w := httptest.NewRecorder()
@@ -76,6 +79,7 @@ func TestListPost(t *testing.T) {
 	fmt.Println(w.Body.String())
 }
 
+// TestCreateComment 创建评论测试
 func TestCreateComment(t *testing.T) {
 	token := login("lisi", "123456")
 	body := bytes.NewBuffer([]byte(`{"content":"这是一段测试评论"}`))
@@ -85,4 +89,36 @@ func TestCreateComment(t *testing.T) {
 	w := httptest.NewRecorder()
 	r.ServeHTTP(w, req)
 	fmt.Println(w.Body.String())
+}
+
+// TestCreateCommentMustLogin 创建文章必须登录
+func TestCreatePostMustLogin(t *testing.T) {
+	body := bytes.NewBuffer([]byte(`{"title":"这是一篇测试文章","content":"这是一篇测试文章的内容"}`))
+	req, _ := http.NewRequest("POST", "/api/post", body)
+	req.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	fmt.Println(w.Body.String()) //{"code":-1,"message":"Unauthorized"}
+}
+
+// TestUpdatePostMustLogin 修改文章必须作者本人
+func TestUpdatePostMustLogin(t *testing.T) {
+	token := login("lisi", "123456")
+	body := bytes.NewBuffer([]byte(`{"title":"这是一篇测试文章","content":"这是一篇测试文章的内容"}`))
+	req, _ := http.NewRequest("PUT", "/api/post/1", body)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+token)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	fmt.Println(w.Body.String()) //{"code":-1,"message":"permission denied"}
+}
+
+// TestDeletePostMustLogin 删除文章必须作者本人
+func TestDeletePostMustLogin(t *testing.T) {
+	token := login("lisi", "123456")
+	req, _ := http.NewRequest("DELETE", "/api/post/1", nil)
+	req.Header.Set("Authorization", "Bearer "+token)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	fmt.Println(w.Body.String()) //{"code":-1,"message":"permission denied"}
 }
